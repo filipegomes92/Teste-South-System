7437fcf5bd17427b0a753f8bcc7d651c883a1eb1
Filipe Gomes <filipe.soaress@hotmail.com>
2024-03-04 04:15:06 -0300

    atualizando pipeline, rodar com pullrequest

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -22,12 +22,31 @@ public class RunCucumberTest {
 
     public static WebDriver driver;
 
-    @BeforeClass
+    /*@BeforeClass
     public static void abreNavegador(){
-        
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+    }*/
+    @BeforeClass
+    public static void setUp() {
+        String downloadFilepath = System.getProperty("downloadFilepath");
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", downloadFilepath);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless"); //should be enabled for Jenkins
+        options.addArguments("--disable-dev-shm-usage"); //should be enabled for Jenkins
+        options.addArguments("--window-size=1920x1080"); //should be enabled for Jenkins
+        driver = new ChromeDriver(options);
+
+        Point point = new Point(-1000, 0);
+        String baseUrl = System.getProperty("testUrl");
+        driver.get(baseUrl);
     }
+
     @AfterClass
     public static void encerraNavegador(){
         driver.quit();
